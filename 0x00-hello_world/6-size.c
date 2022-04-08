6bca231e0cf64496080d1954f3545506e66dcf58
Julius-Java <emmanueljulius9102016@gmail.com>
2022-04-08 06:19:50 -0700

    added extra string to the print function

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,11 +15,11 @@ int main(void)
 	float e;
 
 
-	printf("The size of a char is: %lu.\n", (unsigned long)sizeof(a));
-	printf("THe size of an int is; %lu.\n", (unsigned long)sizeof(b));
-	printf("The size of a long int is: %lu.\n", (unsigned long)sizeof(c));
-	printf("The size of a long long int is: %lu.\n", (unsigned long)sizeof(d));
-	printf("The size of a float is: %lu.\n", (unsigned long)sizeof(e));
+	printf("The size of a char is: %lu byte(s).\n", (unsigned long)sizeof(a));
+	printf("THe size of an int is; %lu byte(s).\n", (unsigned long)sizeof(b));
+	printf("The size of a long int is: %lu byte(s).\n", (unsigned long)sizeof(c));
+	printf("The size of a long long int is: %lu byte(s).\n", (unsigned long)sizeof(d));
+	printf("The size of a float is: %lu byte(s).\n", (unsigned long)sizeof(e));
 
 	return (0);
 }
