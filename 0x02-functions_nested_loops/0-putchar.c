f545f92547969b3ab26c43fcac86215aabb70122
Julius-Java <emmanueljulius9102016@gmail.com>
2022-04-13 06:41:09 -0700

    Description for function main added

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
-/*
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
+/**
+ *  *  *  *  *  *  *  *  *  * main - Entry point
+ *   *   *   *   *   *   *   *   *   *
+ *    *    *    *    *    *    *    *    *    * Return: Always 0 (Success)
+*/
 int main(void)
 {
 	char task_zero[8] = "_putchar";
