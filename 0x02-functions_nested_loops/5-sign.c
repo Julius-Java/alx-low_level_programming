8a1bc1700557bdbc9bbf1d97b27fd2c18be92653
Julius-Java <emmanueljulius9102016@gmail.com>
2022-04-13 14:24:37 -0700

    Task-5 done

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: number to be checked
+ * Return: 1 and '+' if number is greater than 0,
+ * -1 and '-' if the number is less than 0,
+ *  0 and 0 if the number is 0.
+*/
+
+int print_sign(int n)
+{
+	char plus = '+';
+	char zero = '0';
+	char minus = '-';
+
+	if (n > 0)
+	{
+		_putchar(plus);
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar(zero);
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar(minus);
+		return (-1);
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 0-putchar.c - program prints \_putchar on the screen
 1-alphabet.c - program prints alphabet followed by a new line using a custom function 'print_alphabet()'
+2-print_alphabet_x10.c - function prints the alphabet a-z in lowercase in 10 different lines
 
