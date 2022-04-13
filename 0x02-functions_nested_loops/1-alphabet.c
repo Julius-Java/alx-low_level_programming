5fe83d8cce1c705653afba8797aad1a122f4c06a
Julius-Java <emmanueljulius9102016@gmail.com>
2022-04-13 07:14:27 -0700

    Task-1 done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  print_alphabet - prints the alphabet in lowercase
+ *  *   *   *   *   *   *
+ *  loops through all the letters of the alphabet in lowercase
+ *  and prints each one
+ *
+ *  Return: all the letters in the alphabet followed by a new line
+*/
+
+void print_alphabet(void)
+{
+	char alph;
+
+	for (char alph = 'a'; alph <= 'z'; alph++)
+	{
+		_putchar(alph);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #ifndef DEMO_MYHEADER_H
 #define DEMO_MYHEADER_H
 int _putchar(char a);
+void print_alphabet(void);
 #endif
