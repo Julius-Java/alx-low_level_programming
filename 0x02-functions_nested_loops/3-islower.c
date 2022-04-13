1787646cf242d67afb403da1058bfd2ae427c3c3
Julius-Java <emmanueljulius9102016@gmail.com>
2022-04-13 13:46:08 -0700

    Task-3 error fixed

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,14 @@
 #include "main.h"
 
 /**
- * _islower - checks for lower case character
+ * _islower - checks if a character is lowercase
+ * @c: character to be checked
+ * * * * * * * * * * * * * * * *
+ * loops through lower case letters and returns
+ * a value
  *
- */
+ * Return: 1 if the character is lowercase. 0 otherwise
+*/
 
 int _islower(int c)
 {
