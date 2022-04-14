98d5df0344c17a5f1ef858c4460fae3e885f5a2a
Julius-Java <emmanueljulius9102016@gmail.com>
2022-04-13 17:29:12 -0700

    Task-16 done

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *  * main - main block
+ *  * Description: prints fibonacci sequence from 0 to 98
+ *  * Return: 0
+*/
+int main(void)
+{
+int i = 0;
+unsigned long int a = 0, b = 1, next = 0;
+
+while (i < 98)
+{
+	next = a + b;
+	a = b;
+	b = next;
+	printf("%lu", next);
+	if (i < 97)
+	{
+		printf(", ");
+	}
+	i++;
+}
+putchar('\n');
+return (0);
+}
