0bffdd3ac14cd32fbbd4093a93df9457c7009092
Julius-Java <emmanueljulius9102016@gmail.com>
2022-04-14 14:47:41 -0700

    Task-1 done

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+	
+
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+
+	
+}
+
