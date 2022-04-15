33f97cca39f8bf510314f2959531cfa5b17197e8
Julius-Java <emmanueljulius9102016@gmail.com>
2022-04-15 16:18:18 -0700

    Task-5 done

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 - 14 on ten different lines
+ * @line - loop for the number of lines that num prints on
+ * @num - numbers to be printed
+ *
+ * loops through 0 - 10 and prints 0 - 14
+ * for each iteration of the first for loop followed by a new line
+ */
+void more_numbers(void)
+{
+	int line;
+	int num;
+
+	for (line = 0; line <= 10; line++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			_putchar(num + '0');
+		}
+		_putchar('\n');
+	}
+}
