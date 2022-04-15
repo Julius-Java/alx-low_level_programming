4631d76a0305bd0b0d543ba4258f2ae096533692
Julius-Java <emmanueljulius9102016@gmail.com>
2022-04-15 15:44:57 -0700

    Task-2 done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ * @a: input to be multiplied
+ * @b: input to be multiples
+ * Return: product of two inputs
+*/
+int mul(int a, int b)
+{
+	int mult = a * b;
+
+	return (mult);
+}
