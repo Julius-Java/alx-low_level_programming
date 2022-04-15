601ad933e0bacb6164913b3d2994267897f872bd
Julius-Java <emmanueljulius9102016@gmail.com>
2022-04-15 16:39:07 -0700

    Fixed error

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -17,8 +17,16 @@ void more_numbers(void)
 	{
 		for (num = 0; num <= 14; num++)
 		{
-			_putchar(num + '0');
+			if (num < 9)
+			{
+				putchar(num + '0');
+			}
+			if (num > 10)
+			{
+				putchar((num / 10) + '0');
+				putchar((num % 10) + '0');
+			}
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
