a4c24f10cf124b9b4588ee087dc382a28276e714
Julius-Java <emmanueljulius9102016@gmail.com>
2022-04-15 14:51:22 -0700

    Task-0 error fixed

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,14 @@
 #include "main.h"
 
-
-
+/**
+ * _isupper - checks if a character is lowercase
+ * @c: character to be checked
+ * * * * * * * * * * * * * * * *
+ * loops through upper case letters and returns
+ * a value
+ *
+ * Return: 1 if the character is uppercase. 0 otherwise
+*/
 int _isupper(int c)
 {
 	char upper;
