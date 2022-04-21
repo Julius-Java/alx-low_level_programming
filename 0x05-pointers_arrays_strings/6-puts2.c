7713065e7f9392e5df75083cd7413d708cbd3e8a
Julius-Java <emmanueljulius9102016@gmail.com>
2022-04-21 15:47:01 -0700

    Task-6 done

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
- *
+ *puts2 - prints even numbers starting with 0
+ *@str: numbers to be printed
+ *Return: void
  */
 void puts2(char *str)
 {
 	int n;
 
-	for (n = 0; str{n} != '\0'; n++)
+	for (n = 0; str[n] != '\0'; n++)
 	{
 		if (n % 2 == 0)
 		{
