af3a4909b25cc98206e649e5f67a19a610bbbcca
Julius-Java <emmanueljulius9102016@gmail.com>
2022-04-21 16:11:22 -0700

    Task-7 done

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ *puts_half - prints the second half of a string followed by a new line
+ *@str: String to be printed
+ *Return: void
+ */
+void puts_half(char *str)
+{
+	int length;
+
+	length = 0;
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	int half;
+
+	if (length % 2 == 0)
+	{
+		half = length / 2;
+	}
+	else if (length % 2 != 0)
+	{
+		half = (length + 1) / 2;
+	}
+	while (half < length)
+	{
+		_putchar(str[half]);
+		half++
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -5,4 +5,5 @@
 4-print-rev.c - prints string in reverse followed by a new line
 5-rev_string.c - reverses a string
 6-puts2.c - prints all the even numbers in a string starting with 0
+7-puts_half.c - prints half of a string, followed by a new line
 
