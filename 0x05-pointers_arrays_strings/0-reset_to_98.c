2f508ecfb6bca45796d84eebb5a3ac738cf8eb62
Julius-Java <emmanueljulius9102016@gmail.com>
2022-04-21 04:25:50 -0700

    Fixed betty complaints

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "main.h"
-/*
- *rest_to_98 - takes a pointer to an int as a parameter and updates the value to 98
- *
- *(*n) dereferences the location it points to and changes the value to 98
+/**
+ *reset_to_98 - resets any integer to the number 98
+ *@n: integer to be reset
+ *Return: void
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
