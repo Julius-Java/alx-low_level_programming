9a25f2d666a8243cad86599cf5ee2e9468919e31
Julius-Java <emmanueljulius9102016@gmail.com>
2022-04-23 12:58:56 -0700

    Task-2 done

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_strncpy - copies a string from one array to another
+ *@dest: destination array after copying
+ *@src: source array that is copied
+ *@n: number of characters to be copied
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int add;
+
+	for (add = 0; add < n && src[add] != '\0'; add++)
+	{
+		dest[add] = src[add];
+	}
+	for (; add < n; add++)
+	{
+		dest[add] = '\0';
+	}
+	return (dest);
+}
