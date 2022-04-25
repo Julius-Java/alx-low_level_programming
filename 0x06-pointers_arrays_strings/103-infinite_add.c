6f1e170cba301afb24246c7e5d461569d7cfe83c
Julius-Java <emmanueljulius9102016@gmail.com>
2022-04-25 12:47:17 -0700

    Task done

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * infinite_add - adds two numbers
+ * @n1: number to be computed
+ * @n2: number to be computed
+ * @r: buffer to store the result
+ * @size_r: buffer size
+ * Return: char
+*/
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	return (n1 + n2);
+}
