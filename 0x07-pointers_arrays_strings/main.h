41eaf991a27610ce0c6e02baef2a188f641e0914
Julius-Java <emmanueljulius9102016@gmail.com>
2022-04-27 14:33:05 -0700

    Task done

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,4 +5,5 @@ char *_memset(char *s, char b, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
+int _putchar(char c);
 #endif
