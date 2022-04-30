b4f1b7e8ebdafb18cdacad6b0a73f877427250dc
Julius-Java <emmanueljulius9102016@gmail.com>
2022-04-29 17:47:24 -0700

    Task done

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *factorial - function finds the factorial of a given number
+ *@n: factorial number
+ *Return: factorial of number
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,4 +1,5 @@
 0-puts_recursion.c - function prints a string followed by new line using recursion
 1-print_rev_recursion.c - function prints string in reverse using recursion
 2-strlen_recursion.c - function prints the length of a string
+3-factorial.c - returns the factorial of a given number
 
