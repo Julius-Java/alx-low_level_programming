07a91c5c2e48353bd7366ddd928dde3135d196cb
Julius-Java <emmanueljulius9102016@gmail.com>
2022-05-03 13:10:01 -0700

    Task done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,9 +2,10 @@
 /**
  *main - recieves the program name as a parameter and prints it out
  *@argc: argument count
- *@argv[]: array of strings 
+ *@argv: array of strings
+ *Return: 0
  */
-int main(int argc _attribute_((unused)), char *argv[])
+int main(int __attribute__ ((unused)) argc, char *argv[])
 {
 	printf("%s", argv[0]);
 	return (0);
