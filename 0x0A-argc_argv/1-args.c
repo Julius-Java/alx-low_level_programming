2c9444e929503e5ee312a662346c850c1bc7375e
Julius-Java <emmanueljulius9102016@gmail.com>
2022-05-03 13:24:57 -0700

    Task done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - function prints the number of arguments passed to it
+ *@argc: variable holds the number of arguments passed to the function
+ *@argv: variable holds the arguments/string value
+ *Return: 0
+ */
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,2 +1,3 @@
 0-whatsmyname.c - prints it's name, followed by a new line
+1-args.c - prints the number of functions passed to it
 
