c4b2f1e0e773e25079031469d866d2f6ae44afa8
Julius-Java <emmanueljulius9102016@gmail.com>
2022-05-10 13:31:31 -0700

    2-calloc.c task updated

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,32 +1,29 @@
+ #include "main.h"
 #include <stdlib.h>
-#include "main.h"
+
 /**
- *_calloc - function allocates memory space to an array using malloc
- *@nmemb: number of bytes to be allocated
- *@size: data type of number of bytes to be allocated
- *Return: pointer to the allocated memory space
+ * * _calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
  */
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *ptr;
-	unsigned int count;
-
-	ptr = malloc(nmemb * sizeof(size));
+	char *p;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
-	{
 		return (NULL);
-	}
-	else if (ptr == NULL)
-	{
+
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
 		return (NULL);
-	}
-	else
-	{
-		for (count = 0; count < (nmemb * size); count++)
-		{
-			ptr[count] = 0;
-		}
-	}
-	return (ptr);
+
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
+
+	return (p);
 }
