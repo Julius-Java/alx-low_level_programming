9739f7bd8be18baa3ac686d9b0ee1baf4d78b4a2
Julius-Java <emmanueljulius9102016@gmail.com>
2022-05-10 06:07:42 -0700

    function allocates memory to an array using malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ *_calloc - function allocates memory space to an array using malloc
+ *@nmemb: number of bytes to be allocated
+ *@size: data type of number of bytes to be allocated
+ *Return: pointer to the allocated memory space
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *ptr;
+	unsigned int count;
+
+	ptr = malloc(nmemb * sizeof(size));
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	else if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (count = 0; count < nmemb; count++)
+		{
+			ptr[count] = 0;
+		}
+	}
+	return (ptr);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,3 +1,4 @@
 0-malloc_checked.c - function allocates memory using malloc
 1-string_nconcat.c - function concatenates two strings using
+2-calloc.c - function allocates memory space for an array using malloc
 
