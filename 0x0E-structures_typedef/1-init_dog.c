509acce0e9d0eedffdaa53f5cfcdcb26f9bf21f6
Julius-Java <emmanueljulius9102016@gmail.com>
2022-05-11 15:45:04 -0700

    1-init_dog task done

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ *init_dog - function initiablizes a variable type of struct dong
+ *@d: data structure
+ *@name: dog's name
+ *@age: dog's age
+ *@owner: dog's owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
