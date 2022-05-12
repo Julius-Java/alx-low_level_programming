7c4c4e5f20703007d81b27b8af3846ebb26187e2
Julius-Java <emmanueljulius9102016@gmail.com>
2022-05-12 04:38:47 -0700

    Task updated

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,16 +7,21 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		exit(98);
-
-	if (d->name == NULL)
-		printf("nil");
-
-	else if (d->owner == NULL)
-		printf("nil");
-	else
-		printf("Name: %s\n", d->name);
-		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+		{
+			printf("nil");
+		}
+		else if (d->owner == NULL)
+		{
+			printf("nil");
+		}
+		else
+		{
+			printf("Name: %s\n", d->name);
+			printf("Age: %f\n", d->age);
+			printf("Owner: %s\n", d->owner);
+		}
+	}
 }
