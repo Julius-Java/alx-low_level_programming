64f7b8d741d6ffc7fe86823e99397f2327310023
Julius-Java <emmanueljulius9102016@gmail.com>
2022-05-12 04:22:02 -0700

    2-print_dog task done

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ *print_dog - function prints struct dog
+ *@d: pointer to struct dog
+ *Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		exit(98);
+
+	if (d->name == NULL)
+		printf("nil");
+
+	else if (d->owner == NULL)
+		printf("nil");
+	else
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,2 +1,4 @@
 dog.h - contains a struct data type with 3 components
+1-init_dog.c - initializes a structure called struct dog
+2-print_dog.c - prints struct dog
 
