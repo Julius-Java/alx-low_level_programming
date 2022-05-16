4dd7918dda4537d485700566454d31c965a05379
Julius-Java <emmanueljulius9102016@gmail.com>
2022-05-16 11:55:16 -0700

    0-sum_them_all task done

---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,2 @@
+0-sum_them_all.c - variadic function returns the sum of all it's parameters
+
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,5 @@
+#ifndef VARIADIC-FUNC_H
+#define VARIADIC-FUNC_H
+int sum_them_all(const unsigned int n, ...);
+#include <stdarg.h>
+#endif
