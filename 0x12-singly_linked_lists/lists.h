14212a6c8fb7d9f7dad3ce6797cc02991229be0e
Julius-Java <emmanueljulius9102016@gmail.com>
2022-05-31 09:00:27 -0700

    created main.c, main.h, binary_to_unit, and readme files respectively

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -14,9 +14,8 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+#include <stddef.h>
 size_t print_list(const list_t *h);
-#include <stdlib.h>
 int _putchar(char c);
 size_t list_len(const list_t *h);
-#include <stddef.h>
 #endif
