d1f246fe8baa69966e271f31a8092c15dc20110a
Julius-Java <emmanueljulius9102016@gmail.com>
2022-06-16 07:29:32 -0700

    1-list_len.c file updated

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,17 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
+
 /**
- *list_len - function returns the number of elements in a linkeed list
- *@h: pointer to first node
- *Return: number of elements in the list
+ * list_len - function that returns the number of elements in a linked
+ * list_t list
+ * @h: header pointer
+ * Return: number of elements
  */
+
 size_t list_len(const list_t *h)
 {
-	size_t num_element = 0;
+	size_t cont = 0;
 
 	while (h != NULL)
 	{
-		num_element++;
 		h = h->next;
+		cont++;
 	}
-	return (num_element);
+	return (cont);
 }
