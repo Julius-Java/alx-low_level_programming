a867a77280dfb2149c3b53610d272dbd5b09c3e8
Julius-Java <emmanueljulius9102016@gmail.com>
2022-07-08 18:07:17 -0700

    0-main.py file added and 0-print_dlistint.c file modified

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,14 +6,16 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	dlistint_t *temp = h;
+	const dlistint_t *temp = h;
 	size_t node_counter;
+
+	node_counter = 0;
 
 	while (temp != NULL)
 	{
 		printf("%d\n", temp->n);
 		temp = temp->next;
-		node_count++;
+		node_counter++;
 	}
 	return (node_counter);
 }
