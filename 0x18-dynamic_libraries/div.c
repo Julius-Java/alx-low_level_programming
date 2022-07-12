c32a37aa483d2cab08a8a040c54d095de7d6af6e
Julius-Java <emmanueljulius9102016@gmail.com>
2022-07-12 14:26:10 -0700

    div.c file added

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *div- calculates and returns the division of two numbers a and b
+ *@a: left operand
+ *@b: right operand
+ *Return: the result of the division of a and b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
