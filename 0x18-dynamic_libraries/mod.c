cd184eb807322d8e7128a213a5ee91f08736a3e3
Julius-Java <emmanueljulius9102016@gmail.com>
2022-07-12 14:34:25 -0700

    mod.c file added

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *mod - calculates the modulos of two numbers
+ *@a: left operand
+ *@b: right operand
+ *Return: returns the result of the modulos of two numbers
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
