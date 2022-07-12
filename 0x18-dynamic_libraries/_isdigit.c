14f94101b513be12c00972956c66ef2fecb7b2c3
Julius-Java <emmanueljulius9102016@gmail.com>
2022-07-12 08:27:45 -0700

    _isdigit.c file added

---
+++ 0x18-dynamic_libraries/_isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if an input is a digit
+ * @c: input to be checked
+ * Return: 1 if input is a digit, 0 otherwise
+*/
+int _isdigit(int c)
+{
+	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		if (c == digit)
+		{
+			return (1);
+		}
+	}
+
+	return (0);
+}
