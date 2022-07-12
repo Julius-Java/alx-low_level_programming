0d3e76379474dd477d8b4bfde597295e4f621a56
Julius-Java <emmanueljulius9102016@gmail.com>
2022-07-12 13:59:23 -0700

    add function file added

---
+++ 0x18-dynamic_libraries/add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *add - function returns the addtion of two numbers
+ *@a: left operand
+ *@b: right operand
+ *Return: the addition of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
