7cf5b3ae0638740f7d446251184fd84779fd68a9
Julius-Java <emmanueljulius9102016@gmail.com>
2022-07-12 14:07:32 -0700

    mul.c file added

---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *mul - function returns the result of the multiplication of a and b
+ *@a: left operand
+ *@b: right operand
+ *Return: the result of a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
