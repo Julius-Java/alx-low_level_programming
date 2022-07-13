dc42936007e41ce8b53eb2d74ac10348dfb951b6
Julius-Java <emmanueljulius9102016@gmail.com>
2022-07-13 05:51:37 -0700

    100-operations.so dynamic library created

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,54 @@
+/**
+ *mul - function multiplies one number by another
+ *@a: left operand
+ *@b: right operand
+ *Return: the result of the operation
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *sub - substracts the right operand from the left operand
+ *@a: left operand
+ *@b: right operand
+ *Return: the result of the operation
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *div - function divides the left operand a by the left b
+ *@a: left operand
+ *@b: right operand
+ *Return: the result of the operation
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *add - function adds two numbers and returns the result of the operation
+ *@a: left operand
+ *@b: right operand
+ *Return: the result of the operation
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *mod - calculates the modulos of two numbers
+ *@a: left operand
+ *@b: right operand
+ *Return: returns the result of the modulos of two numbers
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
